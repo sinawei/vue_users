f84f2df2b30db53afb2392f9f4e7bfb8a98bb12f
sinawei <2295802859@qq.com>
2020-10-28 14:32:16 +0800

    完成用户列表的功能开发

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,39 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login'
-import Home from "../components/Home";
+import Home from '../components/Home'
+import Welcome from '../components/Welcome'
+import Users from '../components/user/Users'
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', component: Login },
-  { path: '/home', component: Home }
+  {
+    path: '/home',
+    component: Home,
+    redirect: '/welcome',
+    children: [
+      { path: '/welcome', component: Welcome },
+      { path: '/users', component: Users }
+    ]
+  }
 ]
 
 const router = new VueRouter({
   routes
 })
 
-//挂载路由导航守卫
-router.beforeEach((to, from, next) =>{
-  //to 将要访问的路径
-  //from 代表从哪个路径跳转而来
-  //next 是一个函数，表示放行
-  if(to.path === '/login') return next()
-  //获取token
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  // from 代表从哪个路径跳转而来
+  // next 是一个函数，表示放行
+  if (to.path === '/login') return next()
+  // 获取token
   const tokenStr = window.sessionStorage.getItem('token')
-  if(!tokenStr) return next('/login')
+  if (!tokenStr) return next('/login')
   next()
 })
 export default router
